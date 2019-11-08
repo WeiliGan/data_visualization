7479a81d2c404cdecf99de8d1721dc150f56769e
WeiliGan <gwl20190610@163.com>
2019-11-08 22:00:57 +0800

    绘制单个D6骰子的直方图

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 
@@ -15,5 +16,16 @@ frequencise = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencise.append(frequency)
+
+# 对结果进行可视化
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencise)
+hist.render_to_file('die_visual.svg')
 
 print(frequencise)
