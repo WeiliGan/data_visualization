3ca3ca785c0c4d36f4a9a43269eee2f65610f49a
WeiliGan <gwl20190610@163.com>
2019-11-08 22:11:53 +0800

    同时掷两个骰子的直方图

--- die_visual.py
+++ die_visual.py
@@ -2,30 +2,32 @@ import pygal
 from die import Die
 
 
-# 创建一个D6
-die = Die()
+# 创建两个D6骰子
+die_1 = Die()
+die_2 = Die()
 
 # 掷几次骰子，并将结果储存在一个列表中
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # 分析结果
 frequencise = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencise.append(frequency)
 
 # 对结果进行可视化
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.title = "Results of rolling two D6 1000 times."
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencise)
+hist.add('D6 + D6', frequencise)
 hist.render_to_file('die_visual.svg')
 
 print(frequencise)
