7fa86f6215645ec811c325f924fefc4b847a4932
WeiliGan <gwl20190610@163.com>
2019-11-09 00:14:51 +0800

    提取并读取数据——获取文件中的最高气温

--- download_data/highs_lows.py
+++ download_data/highs_lows.py
@@ -1,10 +1,14 @@
 import csv
 
+# 从文件中获取最高气温
 filename = 'sitka_weather_07_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    highs = []
+    for row in reader:
+        highs.append(row[1])
+
+    print(highs)
 
