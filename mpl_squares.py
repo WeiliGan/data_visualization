3749236dc92daa530c58e9602eab293b41385857
WeiliGan <gwl20190610@163.com>
2019-11-08 18:01:37 +0800

    模拟多次随机漫步

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 
-
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 plt.plot(input_values, squares, linewidth=5)
@@ -12,6 +11,5 @@ plt.ylabel("Square of Value", fontsize=14)
 
 # 设置刻度标记的大小
 plt.tick_params(axis='both', labelsize=14)
-
 
 plt.show()
--- random_walk.py
+++ random_walk.py
@@ -3,7 +3,6 @@ from random import choice
 
 class RandomWalk():
     """一个生成随机漫步的类"""
-
     def __init__(self, num_points=5000):
         """初始化随机漫步的属性"""
         self.num_points = num_points
@@ -11,7 +10,6 @@ class RandomWalk():
         # 所有漫步都始于(0,0)
         self.x_values = [0]
         self.y_values = [0]
-    
 
     def fill_walk(self):
         """计算随机漫步包含的所有点"""
