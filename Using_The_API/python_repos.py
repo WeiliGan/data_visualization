37408ae4e0cb6a4086851f7e76f52f8778e7ae48
WeiliGan <gwl20190610@163.com>
2019-11-10 02:36:02 +0800

    根据数据绘图——可以根据数据变化来生成直方图

--- Using_The_API/python_repos.py
+++ Using_The_API/python_repos.py
@@ -14,10 +14,15 @@ print("Total repostoeies:", response_dict['total_count'])
 # 探索有关仓库的信息
 repo_dicts = response_dict['items']
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': str(repo_dict['description']),
+    }
+    plot_dicts.append(plot_dict)
 
 # 可视化
 my_style = LS('#333366', base_style=LCS)
@@ -32,10 +37,9 @@ my_config.truncate_label = 15
 my_config.show_y_guides = False
 my_config.width = 1000
 
-
 chart = pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred python Projects on Github'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
 chart.render_to_file('python_repos.svg')
