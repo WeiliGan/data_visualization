2e97a8a533124991aedf250ffcbb583ee5bcfd79
WeiliGan <gwl20190610@163.com>
2019-11-10 00:40:28 +0800

    概述最受欢迎的仓库

--- Using_The_API/python_repos.py
+++ Using_The_API/python_repos.py
@@ -1,5 +1,6 @@
-import requests
+# -*- coding: utf-8 -*-
 
+import requests
 
 # 执行API调用并存储响应
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
@@ -15,12 +16,11 @@ repo_dicts = response_dict['items']
 print("Repositories returned:", len(repo_dicts))
 
 # 研究第一个仓库
-repo_dict = repo_dicts[0]
+
 print("\n Selected information about first repository:")
-print('Name:', repo_dict['name'])
-print('Owner:', repo_dict['owner']['login'])
-print('Start:', repo_dict['stargazers_count'])
-print('Repository:', repo_dict['html_url'])
-print('Created:', repo_dict['created_at'])
-print('updated:', repo_dict['updated_at'])
-print('Description:', repo_dict['description'])
+for repo_dict in repo_dicts:
+    print('Name:', repo_dict['name'])
+    print('Owner:', repo_dict['owner']['login'])
+    print('Start:', repo_dict['stargazers_count'])
+    print('Repository:', repo_dict['html_url'])
+    print('Description:', repo_dict['description'])
