9134b927ccd3494720429aca0ca0f3c4a43d843c
WeiliGan <gwl20190610@163.com>
2019-11-09 18:23:40 +0800

    绘制收盘价折线图

--- download_data/btc_close_2017.py
+++ download_data/btc_close_2017.py
@@ -3,6 +3,7 @@ from __future__ import (absolute_import, division, print_function,
 
 from urllib.request import urlopen
 import json
+import pygal
 
 json_url = 'https://raw.githubusercontent.com/muxuezi/btc/master/btc_close_2017.json'
 response = urlopen(json_url)
@@ -16,17 +17,40 @@ file_urllib = json.loads(req)
 # print(file_urllib)
 """---------------------------"""
 
-
 # 将数据加载到一个列表中
 filename = 'btc_close_2017_urllib.json'
 with open(filename) as f:
-    btc_date = json.load(f)
+    btc_data = json.load(f)
+
 # 打印每一天信息
-for btc_dict in btc_date:
+for btc_dict in btc_data:
     date = btc_dict['date']
     month = int(btc_dict['month'])
     week = int(btc_dict['week'])
     weekday = btc_dict['weekday']
     close = int(float(btc_dict['close']))
-    print("{} is month {} week {}, {}, the close price is {} RMB".format(
-        date, month, week, weekday, close))
+    # print("{} is month {} week {}, {}, the close price is {} RMB".format(
+    #     date, month, week, weekday, close))
+
+# 创建五个列表，分别存储日期和收盘价
+dates = []
+months = []
+weeks = []
+weekdays = []
+close = []
+# 每一天的信息
+for btc_dict in btc_data:
+    dates.append(btc_dict['date'])
+    months.append(int(btc_dict['month']))
+    weeks.append(int(btc_dict['week']))
+    weekdays.append(btc_dict['weekday'])
+    close.append(int(float(btc_dict['close'])))
+
+line_chart = pygal.Line(x_label_rotatioon=20, show_minor_x_labels=False)
+line_chart.title = '收盘价 (￥)'
+line_chart.x_labels = dates
+# X轴坐标每隔二十天显示一次
+N = 20
+line_chart.x_labels_major = dates[::N]
+line_chart.add('收盘价', close)
+line_chart.render_to_file('收盘价折线图 (￥).svg')
